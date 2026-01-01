4f0a7c43717cdf20980938b84dec84f161975aa6
icecoockie <cocojayni@gmail.com>
2026-01-01 16:39:45 +0800

    Site updated: 2026-01-01 16:39:45

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/24/c语言指针复习/","2025/01/22/hello-world/"];function toRandomPost(){
+var posts=["2025/01/22/hello-world/","2025/01/24/c语言指针复习/","2026/01/01/二分查找/","2026/01/01/计算机复试——二分查找/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
